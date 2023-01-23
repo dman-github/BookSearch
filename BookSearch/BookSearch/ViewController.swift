8151e71da229bb9d3ad6273aa883630be6f51e8f
Damitha Arachchige <Damitha.Arachchige@sensium.co.uk>
2023-01-23 15:35:01 +0000

    initial commit and removal of scene delegate

---
+++ BookSearch/BookSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookSearch
+//
+//  Created by Damitha Arachchige on 23/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
