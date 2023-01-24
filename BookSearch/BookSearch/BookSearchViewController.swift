e2a0c34a01780b25ed6493fd492b68c265fff6e7
Damitha Arachchige <Damitha.Arachchige@sensium.co.uk>
2023-01-24 15:36:42 +0000

    adding the collectionview and cells hooking up the storyboard components to the viewController

--- BookSearch/BookSearch/BookSearchViewController.swift
+++ BookSearch/BookSearch/BookSearchViewController.swift
@@ -7,48 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var activityView: UIActivityIndicatorView!
+class BookSearchViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var searchBarView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let service = OpenLibraryApiServiceImpl()
+       /* let service = OpenLibraryApiServiceImpl()
         service.searchBooks(with: "The lord of the rings") {result in
             switch result {
                 case .success(let obj):
                     let books = obj.docs
-                    let isbn = books?.first?.isbn ?? []
-                    print(isbn.first)
-                    service.loadLargeImage(withISBN: isbn.first ?? "") {[weak self] result in
+                    for i in 0..<10 {
+                        let isbn = books![i].isbn ?? []
+                        print(isbn.first)
+                        service.loadLargeImage(withISBN: isbn.first ?? "") {[weak self] result in
                             switch result {
                                 case .success(let data):
-                                    DispatchQueue.main.async {
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + Double.random(in: 0...10)) {
                                         self?.imageView.image = UIImage(data: data)
                                         self?.activityView.stopAnimating()
                                     }
                                 case .failure(let error):
                                     print(error.localizedDescription)
                             }
+                        }
                     }
                 case .failure(let error):
                     print(error.localizedDescription)
             }
-        }
-        /*
-        self.service.loadLargeImage(withISBN: self.isdn) {[weak self] result in
-                switch result {
-                    case .success(let data):
-                        DispatchQueue.main.async {
-                            self?.imageView.image = UIImage(data: data)
-                            self?.activityView.stopAnimating()
-                        }
-
-                        
-                    case .failure(let error):
-                        print(error.localizedDescription)
-                }
         }*/
     }
 
---
+++ BookSearch/BookSearch/Views/BookSearchCell.swift
@@ -0,0 +1,16 @@
+//
+//  BookSearchCellCollectionViewCell.swift
+//  BookSearch
+//
+//  Created by Damitha Arachchige on 24/01/2023.
+//
+
+import UIKit
+
+class BookSearchCell: UICollectionViewCell {
+    @IBOutlet weak var activityView: UIActivityIndicatorView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageContentView: UIView!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var bookInfoView: UIView!
+}
